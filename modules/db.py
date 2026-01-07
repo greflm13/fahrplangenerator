876fe5f209b1ca371145f6872b97da5686ba7bda
Florian Greistorfer <flo.greistorfer@sorogon.eu>
2026-01-07 16:00:49 +0100

    attempt

---
+++ modules/db.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import sqlite3
+from collections import namedtuple
+
+
+def namedtuple_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    cls = namedtuple("Row", fields)
+    return cls._make(row)
+
+
+con = sqlite3.connect("gtfs.db")
+con.row_factory = namedtuple_factory
+cur = con.cursor()
+
+
+def load_gtfs(folder: str) -> None:
+    """Load GTFS data."""
+    if os.path.isdir(folder):
+        files = os.listdir(folder)
+        for file in files:
+            typ, ext = os.path.splitext(os.path.basename(file))
+            if ext == ".txt":
+                data_path = os.path.join(folder, file)
+                with open(data_path, "r", encoding="utf-8-sig") as f:
+                    csvdata = csv.reader(f, skipinitialspace=True)
+                    csvlines = []
+                    for row in csvdata:
+                        csvlines.append(row)
+                    header = csvlines[0]
+                    data = csvlines[1:]
+                    lines = []
+                    con.execute(f"CREATE TABLE IF NOT EXISTS {typ}({','.join(header)})")
+                    for line in data:
+                        line = "'" + "','".join(line) + "'"
+                        lines.append(line)
+                    con.executemany(f"INSERT INTO {typ} VALUES (?)", data)
+                    con.commit()
+
+
+load_gtfs("/home/user/Documents/20251217-0224_gtfs_evu_2026")
